a6d818f35550b651ae552aea66627ba1f7855d9d
prakau <prakau@doctor.upv.es>
2025-04-27 15:42:01 +0530

    Fix routing and add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './context/ThemeContext';
-import BaseLayout from './layouts/BaseLayout';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { ThemeProvider } from './context/themeContext';
+import BaseLayout from './layout/BaseLayout';
 import HomePage from './pages/Home';
 import AboutPage from './pages/About';
-import ProductsPage from './pages/Products';
+import ProductPage from './pages/Product';
 import TechnologyPage from './pages/Technology';
 import AgriAssistantPage from './pages/AgriAssistant';
 import './styles/global.css';
@@ -14,10 +14,19 @@ const App = () => {
     <Router basename="/">
       <ThemeProvider>
         <BaseLayout>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/products">Products</Link></li>
+              <li><Link to="/technology">Technology</Link></li>
+              <li><Link to="/agri-assistant">Agri Assistant</Link></li>
+            </ul>
+          </nav>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
-            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/products" element={<ProductPage />} />
             <Route path="/technology" element={<TechnologyPage />} />
             <Route path="/agri-assistant" element={<AgriAssistantPage />} />
           </Routes>
